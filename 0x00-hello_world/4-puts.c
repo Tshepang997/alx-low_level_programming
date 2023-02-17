cba4019da53a7c8b47de9354275e5389f4e5cb9f
Tshepang997 <tshepangmokgadi54@gmail.com>
2023-02-17 00:10:02 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: the value should end with
+ */
+
+int main(void)
+{
+	puts(*\"Programming is like building a multilingual puzzle");
+	return (0);
+}
