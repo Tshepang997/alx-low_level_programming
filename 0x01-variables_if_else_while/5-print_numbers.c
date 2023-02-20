f20393e654158e9311c111e3bbd1be2293acc645
Tshepang997 <tshepangmokgadi54@gmail.com>
2023-02-19 21:19:24 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print all the single numbers
+ *
+ * Return: successfully 0
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
