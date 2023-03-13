f1ec35079783bbde4f77fd229d6e54399c88aa3c
Tshepang997 <tshepangmokgadi54@gmail.com>
2023-03-13 06:01:40 -0700

    a library is not a luxury

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to be check
+ *
+ * Return: 0 if succesful
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds first matching char in string
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ * Return: pointer to matching char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
