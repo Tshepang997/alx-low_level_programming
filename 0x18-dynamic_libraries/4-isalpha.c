5a7be898af9f3f83ad18b5fd2945cc3ac7c98b43
Tshepang997 <tshepangmokgadi54@gmail.com>
2023-08-21 23:15:51 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the char should be checked
+ *
+ * Return: 1 if the char is a letter or lowercase otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the value of an integer
+ *@n: the value of int to be computed
+ *
+ * Return: value of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
